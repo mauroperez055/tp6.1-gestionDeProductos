86dd4ce09db8ef8d940da391148d6a1ef88a87f3
Mauro Gabriel Perez <mauroperez055@gmail.com>
2026-09-16 20:11:01 -0300

    vista principal creada

--- src/Clases/Producto.java
+++ src/Clases/Producto.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Clases;
 
-/**
- *
- * @author Mauro Perez
- */
 public class Producto {
+    private String nombre;
+    private Double precio;
+    private String categoria;
+
+    public Producto(String nombre, Double precio, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+    
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Product\n" + 
+                "nombre: " + nombre + 
+                ", \nprecio: " + precio;
+    }
     
 }
